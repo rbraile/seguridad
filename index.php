1354a72bd5189d9cd09b4162ff81ad4d7d0a2f28
Sandra <sp.sanabria@gmail.com>
2016-05-08 19:38:54 -0300

    'se estila pedorramente el agregado de comentarios'

--- index.php
+++ index.php
@@ -44,67 +44,78 @@
             $semanas = $semana->getAllSemanas();
 
         ?>
-        <div class="container">
-        <div class="panel panel-default">
-            <div class="panel-heading">Semanas anteriores del historial de precios del <?php echo $semanaCurrent['fecha_ini'] ?> al <?php echo $semanaCurrent['fecha_fin'] ?></div>
-                <ul class="list-group">
-                    <?php foreach ($semanas as $key => $value) {?>
-                        <?php if($value["id"] != $semanaCurrent["id"]):?>
-                            <li class="list-group-item"><a href="/precio-semana.php?idSemana=<?php echo $value["id"];?>"><?php echo $value["fecha_ini"] . " - " . $value["fecha_fin"];?></a></li>
-                        <?php endif;?>
-                    <?php }?>
-                </ul>
-            </div>
-        </div>
 
         <div class="container">
-        <div class="panel panel-default">
-            <!-- Default panel contents -->
-            <div class="panel-heading">Precios de semanas actual</div>
-                <?php
-                    $idSemana = $semanaCurrent["id"];
+            <div class="row">
+                <div class="col-md-6">
+                     <div class="panel panel-default">
+                                <!-- Default panel contents -->
+                                <div class="panel-heading">Precios la semana actual</div>
+                                    <?php
+                                        $idSemana = $semanaCurrent["id"];
 
-                    $precio = new Precio();
-                    $precios = $precio->getPreciosHistorial($idSemana);   
-                ?>
-                <table class="table">
-                    <thead>
-                    <?php if($precios != 0): ?>
-                        <tr>
-                            <th>Producto</th>
-                            <th>Maximo</th>
-                            <th>Minimo</th>
-                            <th>promedio</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <?php foreach ($precios as $key => $value) {?>
-                            <tr>
-                                <td><strong><?php echo $key;?></strong></td>
-                                <td><?php echo $value['maximo'];?></td>
-                                <td><?php echo $value['minimo'];?></td>
-                                <td><?php echo ($value['sumaTotal']/$value['contador']);?></td>
-                                
-                            </tr>
-                    
-                        <?php }?>
-                        </tbody>
-                    <?php else:?>
-                        <h1>No hay resultados para mostrar</h1>
-                    <?php endif;?>
-                </table>
+                                        $precio = new Precio();
+                                        $precios = $precio->getPreciosHistorial($idSemana);   
+                                    ?>
+                                    <table class="table">
+                                        <thead>
+                                        <?php if($precios != 0): ?>
+                                            <tr>
+                                                <th>Producto</th>
+                                                <th>Maximo</th>
+                                                <th>Minimo</th>
+                                                <th>promedio</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <?php foreach ($precios as $key => $value) {?>
+                                                <tr>
+                                                    <td><strong><?php echo $key;?></strong></td>
+                                                    <td><?php echo $value['maximo'];?></td>
+                                                    <td><?php echo $value['minimo'];?></td>
+                                                    <td><?php echo ($value['sumaTotal']/$value['contador']);?></td>
+                                                    
+                                                </tr>
+                                        
+                                            <?php }?>
+                                            </tbody>
+                                        <?php else:?>
+                                            <h1>No hay resultados para mostrar</h1>
+                                        <?php endif;?>
+                                    </table>
+                         </div> 
+                      
+                </div>
+                 <div class="col-md-6">
+                     <div class="panel panel-default">
+                        <!-- Default panel contents -->
+                        <div class="panel-heading">Cometarios de la aplicación</div>
+                        <div class="">
+                            <form action="comentario.php" method="POST">
+                                <label class="cometarioLabel">Comentario</label>
+                                <textarea name="comentario" class="form-control" rows="3">
+                                    
+                                </textarea>
+                                <input type="submit"  class="btn btn-default " value="Comentar" name="comentar" />
+                            </form>
+                        </div>
+                    </div>
+
+                 </div>
             </div>
-            <div class="panel panel-default">
-                <!-- Default panel contents -->
-                <div class="panel-heading">Cometarios de la aplicación</div>
-                <div class="">
-                    <form action="comentario.php" method="POST">
-                        <label>Comentario</label>
-                        <textarea name="comentario">
-                            
-                        </textarea>
-                        <input type="submit" value="Comentar" name="comentar" />
-                    </form>
+            <div class="row">
+                <div class="col-md-12">
+                      <div class="panel panel-default">
+                            <div class="panel-heading">Semanas anteriores del historial de precios del <?php echo $semanaCurrent['fecha_ini'] ?> al <?php echo $semanaCurrent['fecha_fin'] ?></div>
+                                <ul class="list-group">
+                                    <?php foreach ($semanas as $key => $value) {?>
+                                        <?php if($value["id"] != $semanaCurrent["id"]):?>
+                                            <li class="list-group-item"><a href="/precio-semana.php?idSemana=<?php echo $value["id"];?>"><?php echo $value["fecha_ini"] . " - " . $value["fecha_fin"];?></a></li>
+                                        <?php endif;?>
+                                    <?php }?>
+                                </ul>
+                        </div>
+                        
                 </div>
             </div>
         </div>
